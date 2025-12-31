ba745d5f3d9a2ec83ca9bd252c5130f33b21ce62
Lilo <linoallg@schafferts.ch>
2025-12-31 10:30:29 +0100

    finished Debugging and added relative linenumbers

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,6 +7,21 @@ vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-
 vim.keymap.set("n", "<leader>nt", ":Neotree filesystem reveal right<CR>")
+
+vim.keymap.set('n', '<leader>fn', function()
+  local snippet = [[#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int int64_t
+
+signed main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    return 0;
+}]]
+  vim.api.nvim_put(vim.split(snippet, '\n'), 'l', true, true)
+end, { desc = "Insert Lua function template" })
 
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -2,3 +2,5 @@ vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set expandtab")
 vim.cmd("set softtabstop=4")
+vim.opt.number = true
+vim.opt.relativenumber = true
