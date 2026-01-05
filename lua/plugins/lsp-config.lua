d0f0349a6965948dcaab6eb68b9aa49724ddde08
Lilo <linoallg@schafferts.ch>
2026-01-05 16:15:21 +0100

    changed something in lspconfig

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,93 +1,91 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
+	{
+		"mason-org/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = {
-        "lua_ls",
-        "bashls",
-        "hyprls",
-        "pyright",
-        "cssls",
-        "cmake",
-        "clangd",
-        "codelldb",
-        "clang_format",
-      },
-    },
-  },
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				"bashls",
+				"hyprls",
+				"pyright",
+				"cssls",
+				"cmake",
+				"clangd",
+				"codelldb",
+				"clang_format",
+			},
+		},
+	},
 
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities =
-        require("cmp_nvim_lsp").default_capabilities()
-        vim.diagnostic.config({
-            virtual_text = true,   -- show message inline
-            signs = true,          -- show symbols in the gutter
-            underline = true,      -- underline problematic code
-            update_in_insert = false,
-            severity_sort = true,
-        })
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			vim.diagnostic.config({
+				virtual_text = true, -- show message inline
+				signs = true, -- show symbols in the gutter
+				underline = true, -- underline problematic code
+				update_in_insert = false,
+				severity_sort = true,
+			})
 
+			-- Define servers (NO lspconfig)
+			vim.lsp.config("lua_ls", {
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			})
 
-      -- Define servers (NO lspconfig)
-      vim.lsp.config("lua_ls", {
-        capabilities = capabilities,
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" },
-            },
-          },
-        },
-      })
+			vim.lsp.config("bashls", {
+				capabilities = capabilities,
+			})
 
-      vim.lsp.config("bashls", {
-        capabilities = capabilities,
-      })
+			vim.lsp.config("hyprls", {
+				capabilities = capabilities,
+			})
 
-      vim.lsp.config("hyprls", {
-        capabilities = capabilities,
-      })
+			vim.lsp.config("pyright", {
+				capabilities = capabilities,
+			})
 
-      vim.lsp.config("pyright", {
-        capabilities = capabilities,
-      })
+			vim.lsp.config("cssls", {
+				capabilities = capabilities,
+			})
 
-      vim.lsp.config("cssls", {
-        capabilities = capabilities,
-      })
+			vim.lsp.config("cmake", {
+				capabilities = capabilities,
+			})
+			vim.lsp.config("clangd", {
+				cmd = { "clangd", "--query-driver=/usr/bin/clang++,/usr/bin/g++" },
+				capabilities = capabilities,
+			})
 
-      vim.lsp.config("cmake", {
-        capabilities = capabilities,
-      })
-      vim.lsp.config("clangd", {
-        capabilities = capabilities
-      })
+			-- Enable servers
+			vim.lsp.enable({
+				"lua_ls",
+				"bashls",
+				"hyprls",
+				"pyright",
+				"cssls",
+				"cmake",
+			})
 
-      -- Enable servers
-      vim.lsp.enable({
-        "lua_ls",
-        "bashls",
-        "hyprls",
-        "pyright",
-        "cssls",
-        "cmake",
-      })
-
-      -- Keymaps
-      local keymap = vim.keymap
-      keymap.set("n", "<leader>ki", vim.lsp.buf.hover)
-      keymap.set("n", "<leader>gd", vim.lsp.buf.definition)
-      keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
-    end,
-  },
+			-- Keymaps
+			local keymap = vim.keymap
+			keymap.set("n", "<leader>ki", vim.lsp.buf.hover)
+			keymap.set("n", "<leader>gd", vim.lsp.buf.definition)
+			keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
+		end,
+	},
 }
-
