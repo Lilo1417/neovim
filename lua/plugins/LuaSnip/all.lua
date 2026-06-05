cd48a4287c711a3baa84d0104bd43865ab1ef8f8
lilo <linoallg@schafferts.ch>
2026-06-05 11:55:05 +0200

    Removed the hi snippet

--- lua/plugins/LuaSnip/all.lua
+++ lua/plugins/LuaSnip/all.lua
@@ -1,18 +1,4 @@
 return {
   -- A snippet that expands the trigger "hi" into the string "Hello, world!".
-  require("luasnip").snippet(
- { -- Table 1: snippet parameters
-      trig="hi",
-      dscr="An autotriggering snippet that expands 'hi' into 'Hello, world!'",
-      regTrig=false,
-      priority=100,
-      snippetType="autosnippet"
-    },
-    { -- Table 2: snippet nodes (don't worry about this for now---we'll cover nodes shortly)
-      t("Hello, world!"), -- A single text node
-    }
-    -- Table 3, the advanced snippet options, is left blank.
-
-  ),
 
 }
